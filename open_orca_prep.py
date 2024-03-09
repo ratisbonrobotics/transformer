efc8597d1b5b123ba448577038907cf7609d73eb
Markus Heimerl <markusheimerl@hotmail.com>
2024-03-09 01:57:44 +0000

    🛠️ .gitignore -> Added open_orca.pkl 🟢 open_orca_prep.py -> New file, processes dataset

---
+++ open_orca_prep.py
@@ -0,0 +1,16 @@
+from datasets import load_dataset
+import pickle
+
+# Load the dataset
+dataset = load_dataset("Open-Orca/OpenOrca", cache_dir="/dev/shm/")
+
+# Function to apply modification
+def apply_modification(row):
+    row["data"] = "[USER] " + row["question"].lower() + " [ASSISTANT] " + row["response"].lower()
+    return row
+
+modified_dataset = dataset.map(apply_modification, num_proc=8, remove_columns=["id", "system_prompt", "question", "response"])
+
+with open("open_orca.pkl", "wb") as file:
+    pickle.dump(modified_dataset["train"]["data"], file)
+
