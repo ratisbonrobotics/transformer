21f60544d68248ec7eee10be555005b3ed501f65
Markus Heimerl <markusheimerl@hotmail.com>
2024-03-13 09:45:08 +0000

    🛠️ model.py -> Replaced RMSNorm with LayerNorm

--- model.py
+++ model.py
@@ -1,14 +1,5 @@
 import torch
 
-class RMSNorm(torch.nn.Module):
-    def __init__(self, dim):
-        super().__init__()
-        self.scale = dim**0.5
-        self.g = torch.nn.Parameter(torch.ones(dim))
-
-    def forward(self, x: torch.Tensor):
-        return torch.nn.functional.normalize(x, dim=-1) * self.scale * self.g
-
 class FeedForward(torch.nn.Module):
     def __init__(self, hidden_dim, ff_dim):
         super().__init__()
@@ -56,8 +47,8 @@ class TransformerBlock(torch.nn.Module):
         super().__init__()
         self.attention = Attention(num_heads, hidden_dim, hidden_dim // num_heads)
         self.feed_forward = FeedForward(hidden_dim, ff_dim)
-        self.attention_norm = RMSNorm(hidden_dim)
-        self.ffn_norm = RMSNorm(hidden_dim)
+        self.attention_norm = torch.nn.LayerNorm(hidden_dim)
+        self.ffn_norm = torch.nn.LayerNorm(hidden_dim)
 
     def forward(self, x: torch.Tensor, mask: torch.Tensor) -> torch.Tensor:
         r = self.attention.forward(self.attention_norm(x), mask)
@@ -72,10 +63,10 @@ class LanguageModel(torch.nn.Module):
         self.tok_emb = torch.nn.Embedding(vocab_size, hidden_dim)
         self.pos_emb = torch.nn.Embedding(seq_len, hidden_dim)
         self.register_buffer("pos", torch.arange(seq_len, dtype=torch.int))
-        self.pos_norm = RMSNorm(hidden_dim)
+        self.pos_norm = torch.nn.LayerNorm(hidden_dim)
         self.register_buffer("mask", torch.triu(torch.ones(seq_len, seq_len), diagonal=1).bool())
         self.transformer_blocks = torch.nn.ModuleList([TransformerBlock(num_heads, hidden_dim, ff_dim) for _ in range(num_blocks)])
-        self.out_norm = RMSNorm(hidden_dim)
+        self.out_norm = torch.nn.LayerNorm(hidden_dim)
         self.out_linear = torch.nn.Linear(hidden_dim, vocab_size, bias=False)
 
         torch.nn.init.normal_(self.tok_emb.weight, mean=0.0, std=0.02)
