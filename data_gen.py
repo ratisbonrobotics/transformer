49f11826588fd8dd7900c4cb6c973cdb579f0726
Markus Heimerl <markusheimerl@hotmail.com>
2024-03-22 14:21:47 +0000

    🛠️ data_gen.py -> Updated data formatting & saving
    🔴 open_orca_cache.pkl.gz.00
    
    🔴 open_orca_cache.pkl.gz.01

--- data_gen.py
+++ data_gen.py
@@ -9,11 +9,11 @@ dataset = load_dataset("Open-Orca/OpenOrca")
 
 # Function to apply modification
 def apply_modification(row):
-    row["data"] = "[USER] " + row["question"].lower() + " [ASSISTANT] " + row["response"].lower()
+    row["data"] = "[SYSTEM] " + row["system_prompt"] + " [USER] " + row["question"] + " [ASSISTANT] " + row["response"]
     return row
 
 modified_dataset = dataset.map(apply_modification, num_proc=8, remove_columns=["id", "system_prompt", "question", "response"])
 
 with open("open_orca.pkl", "wb") as file:
-    pickle.dump(modified_dataset["train"]["data"][:10000], file)
+    pickle.dump(modified_dataset["train"]["data"], file)
 
