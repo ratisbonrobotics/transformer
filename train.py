3767b3834132fc68a34d50669df4b227480762dc
Markus Heimerl <markusheimerl@hotmail.com>
2024-03-22 23:04:22 +0000

    🛠️ train.py -> Reduced BATCH_SIZE to 8 🛠️ train.py -> Halved WARMUP_STEPS to 1000 🛠️ train.py -> Lowered learning_rate in adam 🛠️ train.py -> Simplified L2 regularization in loss_fn

--- train.py
+++ train.py
@@ -12,11 +12,11 @@ from model import language_model, init_params
 
 # Constants
 NUM_EPOCHS = 1000
-BATCH_SIZE = 16
-WARMUP_STEPS = 2000
+BATCH_SIZE = 8
+WARMUP_STEPS = 1000
 WANDB = True
 
-def create_adam_state(params, learning_rate=1e-3, beta_1=0.9, beta_2=0.999, epsilon=1e-8):
+def create_adam_state(params, learning_rate=1e-4, beta_1=0.9, beta_2=0.999, epsilon=1e-8):
     state = {
         "step": 0,
         "learning_rate": learning_rate,
@@ -84,8 +84,7 @@ def loss_fn(learnable_params, inputs, labels, pos, mask, n_heads, scale, vocab_s
     one_hot_labels = jax.nn.one_hot(labels, vocab_size)
     log_softmax_logits = jax.nn.log_softmax(logits, axis=-1)
     loss = -jax.numpy.sum(one_hot_labels * log_softmax_logits) / labels.size
-    l2_regularization = sum(jax.numpy.sum(jax.numpy.square(param)) for param in jax.tree_util.tree_leaves(learnable_params))
-    loss = loss + 0.01 * l2_regularization
+    loss += sum(jax.numpy.sum(jax.numpy.square(param)) for param in jax.tree_util.tree_leaves(learnable_params)) * 1e-4
     return loss * 128.0
 
 # Define training step
