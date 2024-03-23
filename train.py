88e067a018dc6247260d62602b8aabb1ecf30cc1
Markus Heimerl <markusheimerl@hotmail.com>
2024-03-23 13:59:59 +0000

    🛠️ train.py -> Updated JAX initialization with coordinator address

--- train.py
+++ train.py
@@ -1,7 +1,7 @@
 import os
 import jax
 
-jax.distributed.initialize(num_processes=8)
+jax.distributed.initialize(coordinator_address="10.130.0.53:4444", num_processes=8)
 
 import tqdm
 import wandb
