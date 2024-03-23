6eb67f4ebaa0aba460c0e79b89040ebb6234d24a
Markus Heimerl <markusheimerl@hotmail.com>
2024-03-23 15:00:08 +0000

    🛠️ train.py -> Limited wandb init & log to process 0

--- train.py
+++ train.py
@@ -123,7 +123,7 @@ def train_step(learnable_params, inputs, labels, pos, mask, n_heads, scale, voca
 jit_train_step = jax.pmap(train_step, static_broadcasted_argnums=(5,6,7,8))
 
 # Training loop
-if WANDB: wandb.init(project="next")
+if parser.parse_args().process_id == 0 and WANDB: wandb.init(project="next")
 for epoch in range(NUM_EPOCHS):
     indices = list(range(0, len(train_dataset), BATCH_SIZE * jax.device_count()))[:-1]
     random.shuffle(indices)
@@ -141,4 +141,4 @@ for epoch in range(NUM_EPOCHS):
             
             loss, learnable_params, adam_state, learning_rate = jit_train_step(learnable_params, device_batch_inputs, device_batch_labels, static_config['pos'], static_config['mask'], static_config["n_heads"], static_config["scale"], train_dataset.vocab_size, len(indices) * NUM_EPOCHS, adam_state)
             pbar.set_description(f"Epoch {epoch + 1}/{NUM_EPOCHS} - Training Loss: {jax.numpy.mean(loss):.4f} - Learning Rate: {jax.numpy.mean(learning_rate):.10f}")
-            if WANDB: wandb.log({"loss": jax.numpy.mean(loss).item(), "learning_rate": jax.numpy.mean(learning_rate).item()})
+            if parser.parse_args().process_id == 0 and WANDB: wandb.log({"loss": jax.numpy.mean(loss).item(), "learning_rate": jax.numpy.mean(learning_rate).item()})
