422ac5025e9e442ce4b4517dc1b650900b5bd1d7
Markus Heimerl <markusheimerl@hotmail.com>
2024-03-23 16:31:48 +0000

    Increased warmup and batch size

--- train.py
+++ train.py
@@ -20,8 +20,8 @@ from model import language_model, init_params
 
 # Constants
 NUM_EPOCHS = 100
-BATCH_SIZE = 16
-WARMUP_STEPS = 3000
+BATCH_SIZE = 20
+WARMUP_STEPS = 10000
 WANDB = True
 
 def create_adam_state(params, learning_rate=1e-2, beta_1=0.9, beta_2=0.999, epsilon=1e-8):
