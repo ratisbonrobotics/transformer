af6b29153e7c49765a5c999400f958b1959b187b
Markus Heimerl <markusheimerl@hotmail.com>
2024-03-27 14:47:49 +0000

    🛠️ data_gen.py -> Updated data formatting tokens 🛠️ infer.py -> Reordered special tokens 🛠️ open_orca_cache.pkl.gz.aa -> Updated file hash and size 🛠️ open_orca_cache.pkl.gz.ab -> Updated file hash and size 🛠️ open_orca_cache.pkl.gz.ac -> Updated file hash and size 🛠️ train.py -> Reordered special tokens

--- train.py
+++ train.py
@@ -35,7 +35,7 @@ class TextDataset:
             name="cl100k_tokenizer",
             pat_str=r"""'(?i:[sdmt]|ll|ve|re)|[^\r\n\p{L}\p{N}]?+\p{L}+|\p{N}{1,3}| ?[^\s\p{L}\p{N}]++[\r\n]*|\s*[\r\n]|\s+(?!\S)|\s+""",
             mergeable_ranks=load_tiktoken_bpe("https://openaipublic.blob.core.windows.net/encodings/cl100k_base.tiktoken", expected_hash="223921b76ee99bde995b7ff738513eef100fb51d18c93597a113bcffe865b2a7"),
-            special_tokens={"<|endoftext|>": 100257, "<|system|>": 100258, "<|user|>": 100259, "<|assistant|>": 100260}
+            special_tokens={"<|system|>": 100257, "<|user|>": 100258, "<|assistant|>": 100259, "<|endoftext|>": 100260}
         )
 
         self.vocab_size = tokenizer.n_vocab
