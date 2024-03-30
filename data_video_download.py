c9b55774fe5082c28f59d69870d06149dc5ba692
Markus Heimerl <markusheimerl@hotmail.com>
2024-03-30 18:03:55 +0000

    🛠️ data_video_download.py -> Simplified & cleaned up - Removed video distortion functions - Adjusted video download process - Enhanced video download logging

--- data_video_download.py
+++ data_video_download.py
@@ -1,49 +1,22 @@
+import os
 import csv
 import random
-from pytube import YouTube
 import hashlib
-import os
-import cv2
-import numpy as np
-import subprocess
-
-def distort(video_path):
-    cap = cv2.VideoCapture(video_path)
-    fps, num_frames, frame_height, frame_width = cap.get(cv2.CAP_PROP_FPS), int(cap.get(cv2.CAP_PROP_FRAME_COUNT)), int(cap.get(cv2.CAP_PROP_FRAME_HEIGHT)), int(cap.get(cv2.CAP_PROP_FRAME_WIDTH))
-    frames = [cap.read()[1] for _ in range(int(fps * 5)) if cap.isOpened()] # range(num_frames)
-    cap.release()
-    video_tensor = np.array(frames)
-    noisy_video_tensor = np.clip(video_tensor + np.random.normal(0, 50, video_tensor.shape), 0, 255).astype(np.uint8)
-    for frame in noisy_video_tensor:
-        for _ in range(100):
-            x, y = np.random.randint(0, frame_width - 20), np.random.randint(0, frame_height - 20)
-            frame[y:y+20, x:x+20] = 0
-    os.makedirs("videos/distorted/", exist_ok=True)
-    output_path = f"videos/distorted/{video_path.split('/')[-1].split('.')[0]}.mp4"
-    out = cv2.VideoWriter(output_path, cv2.VideoWriter_fourcc(*'mp4v'), fps, (frame_width, frame_height))
-    for frame in noisy_video_tensor:
-        out.write(frame)
-    out.release()
-
-    # Compress the distorted video using ffmpeg
-    compressed_output_path = f"videos/distorted/{video_path.split('/')[-1].split('.')[0]}_compressed.mp4"
-    subprocess.call(['ffmpeg', '-loglevel', 'quiet', '-i', output_path, '-c:v', 'libx264', '-preset', 'medium', '-crf', '23', '-c:a', 'copy', compressed_output_path])
-    # Remove the uncompressed distorted video
-    os.remove(output_path)
+from pytube import YouTube
 
 video_ids = [row['video_id'] for row in csv.DictReader(open('howto100m.csv', 'r'))]
 sampled_ids = random.sample(video_ids, 2)
 for video_id in sampled_ids:
     try:
-        video = YouTube(f'https://www.youtube.com/watch?v={video_id}')
+        url = f'https://www.youtube.com/watch?v={video_id}'
+        video = YouTube(url)
         temp_filename = str(random.randint(0, 2**64-1)) + ".mp4"
-        print(f'Downloading video: {video.title}')
-        video.streams.get_lowest_resolution().download(output_path='videos/original', filename=temp_filename)
-        with open(f'videos/original/{temp_filename}', 'rb') as file:
+        print(f'Downloading video: {video.title} from {url}')
+        video.streams.get_lowest_resolution().download(output_path='videos', filename=temp_filename)
+        with open(f'videos/{temp_filename}', 'rb') as file:
             file_hash = hashlib.sha256(file.read()).hexdigest()
-        original_path, new_path = f'videos/original/{temp_filename}', f'videos/original/{file_hash}.mp4'
+        original_path, new_path = f'videos/{temp_filename}', f'videos/{file_hash}.mp4'
         os.rename(original_path, new_path)
-        distort(new_path)
-        print('Video downloaded successfully.')
+        print(f'Video downloaded successfully')
     except Exception as e:
         print(f'Error downloading video: {video_id}\nError message: {str(e)}')
