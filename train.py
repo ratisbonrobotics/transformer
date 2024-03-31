8ce86f996366666333a8728e2b9e2cb2d0d89be8
Markus Heimerl <markusheimerl@hotmail.com>
2024-03-31 19:36:31 +0000

    🛠️ train.py -> Changed wandb project name to "encoder"

--- train.py
+++ train.py
@@ -92,7 +92,7 @@ def train_step(learnable_params, adam_state, inputs, labels, height_pos, width_p
 jit_train_step = jax.pmap(train_step, static_broadcasted_argnums=(6,7,8,9), axis_name='p')
 
 # Training loop
-if WANDB: wandb.init(project="jax")
+if WANDB: wandb.init(project="encoder")
 for epoch in range(NUM_EPOCHS):
     indices = list(range(epoch, len(train_dataset), BATCH_SIZE * jax.local_device_count()))[:-1]
     random.shuffle(indices)
