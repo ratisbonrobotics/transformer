ae1d056667184f95db8af459d0bed965fe40fc04
Markus Heimerl <markusheimerl@hotmail.com>
2024-04-03 12:51:25 +0000

    🛠️ infer.py -> Renamed function and added JIT compilation

--- infer.py
+++ infer.py
@@ -24,17 +24,16 @@ tokenizer = tiktoken.Encoding(
 )
 
 # Define the inference function
-def generate_text(key, token_ids, temperature=0.7, top_k=40):
+def generate_token(key, token_ids, temperature=0.7, top_k=40):
     logits = language_model(learnable_params, token_ids[None, :], static_config['pos'][:token_ids.shape[0]], static_config['mask'], static_config['n_heads'], static_config['scale'])
     logits = logits[0, -1] / temperature
-    
     top_k_indices = jax.numpy.argsort(logits)[-top_k:]
     top_k_logits = logits[top_k_indices]
     top_k_probs = jax.nn.softmax(top_k_logits)
-
     next_token = jax.random.choice(key, top_k_indices, p=top_k_probs)
-
     return next_token
+
+generate_text_jit = jax.jit(generate_token)
 
 # Infer model, starting from prompt
 prompt = "<|system|> You are an AI assistant. You will be given a task. You must generate a detailed and long answer. <|user|> What happens next in this paragraph? She then rubs a needle on a cotton ball then pushing it onto a pencil and wrapping thread around it. She then holds up a box of a product and then pouring several liquids into a bowl. she Choose your answer from: A. adds saucepan and shakes up the product in a grinder. B. pinches the thread to style a cigarette, and then walks away. C. then dips the needle in ink and using the pencil to draw a design on her leg, rubbing it off with a rag in the end. D. begins to style her hair and cuts it several times before parting the ends of it to show the hairstyle she has created. <|assistant|>"
@@ -43,7 +42,7 @@ key = jax.random.key(random.randint(0, 2**16-1))
 token_ids = jax.numpy.array(tokenizer.encode(prompt, allowed_special="all"), dtype=jax.numpy.uint32)
 for _ in range(50):
     key, round_key = jax.random.split(key)
-    next_token = generate_text(round_key, token_ids)
+    next_token = generate_token(round_key, token_ids)
     token_ids = jax.numpy.append(token_ids, next_token)
     print(tokenizer.decode([next_token]), end="", flush=True)
 
