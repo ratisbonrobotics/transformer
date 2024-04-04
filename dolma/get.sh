88cdc5b89f814ce47af9a367bfc92596b1a3fd0d
Markus Heimerl <markusheimerl@hotmail.com>
2024-04-04 14:12:22 +0000

    🛠️ [.gitattributes] -> LFS filters removed 🛠️ [.gitignore] -> Added *.gz to ignore list 🔴 [open_orca_cache.pkl.gz.aa] 🔴 [open_orca_cache.pkl.gz.ab] 🔴 [open_orca_cache.pkl.gz.ac]

---
+++ dolma/get.sh
@@ -0,0 +1,7 @@
+mkdir -p /home/markusheimerl/xvit-415020_dolma/
+gcsfuse xvit-415020_dolma /home/markusheimerl/xvit-415020_dolma/
+cp /home/markusheimerl/xvit-415020_dolma/books-0000.json.gz /home/markusheimerl/transformer/dolma/ &
+cp /home/markusheimerl/xvit-415020_dolma/books-0001.json.gz /home/markusheimerl/transformer/dolma/ &
+cp /home/markusheimerl/xvit-415020_dolma/books-0002.json.gz /home/markusheimerl/transformer/dolma/ &
+cp /home/markusheimerl/xvit-415020_dolma/en_simple_wiki_v0-0000.json /home/markusheimerl/transformer/dolma/ &
+cp /home/markusheimerl/xvit-415020_dolma/en_simple_wiki_v0-0001.json /home/markusheimerl/transformer/dolma/
