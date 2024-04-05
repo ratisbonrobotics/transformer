3ba2c6af451770b7b2e957ed96dc340a8a760b9f
Markus Heimerl <markusheimerl@hotmail.com>
2024-04-05 14:42:13 +0000

    🛠️ infer.py -> Updated `generate_text_jit` function 🛠️ infer.py -> Added `seq_len` and `mask` setup 🛠️ infer.py -> Increased loop iterations from 50 to 512

--- infer.py
+++ infer.py
@@ -30,17 +30,18 @@ def generate_token(key, token_ids, mask, seq_len, temperature, top_k):
     next_token = jax.random.choice(key, top_k_indices, p=top_k_probs)
     return next_token
 
-generate_text_jit = jax.jit(generate_token)
+generate_text_jit = jax.jit(generate_token, static_argnums=(3,4,5))
 
 # Infer model, starting from prompt
 prompt = "<|system|> You are an AI assistant. You will be given a task. You must generate a detailed and long answer. <|user|> What happens next in this paragraph? She then rubs a needle on a cotton ball then pushing it onto a pencil and wrapping thread around it. She then holds up a box of a product and then pouring several liquids into a bowl. she Choose your answer from: A. adds saucepan and shakes up the product in a grinder. B. pinches the thread to style a cigarette, and then walks away. C. then dips the needle in ink and using the pencil to draw a design on her leg, rubbing it off with a rag in the end. D. begins to style her hair and cuts it several times before parting the ends of it to show the hairstyle she has created. <|assistant|>"
 key = jax.random.key(random.randint(0, 2**16-1))
 token_ids = jax.numpy.array(tokenizer.encode(prompt, allowed_special="all"), dtype=jax.numpy.uint32)
-
+seq_len = 128
+mask = static_config["mask"][:, :, :seq_len, :seq_len]
 start_time = time.time()
-for i in range(50):
+for i in range(512):
     key, round_key = jax.random.split(key)
-    next_token = generate_token(round_key, token_ids[None, :], static_config["mask"][:, :, :token_ids.size, :token_ids.size], token_ids.size, 0.7, 40)
+    next_token = generate_text_jit(round_key, token_ids[None, -seq_len:], mask, seq_len, 0.7, 40)
     token_ids = jax.numpy.append(token_ids, next_token)
     print("\033[2J\033[1;1H", end="")
     print(tokenizer.decode(token_ids), end="", flush=True)
